2a55cb03b228a8e2faaf4991ff6d952f76db660a
Keejay Kim <keejayk@cs.washington.edu>
2024-01-16 02:46:47 -0800

    add function for drawing object

--- space_shenanigans/render_system.py
+++ space_shenanigans/render_system.py
@@ -1,30 +1,49 @@
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import numpy as np
 from space_object import SpaceObject
 from space_system import SpaceSystem
 
 
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
-# class Render:
-#     def __init__(self, size):
-#         fig, ax = plt.subplots(1, 1, subplot_kw={"projection": "3d"})
-#         self.size = size
-#         self.objects = []
-#         self.figure = fig
-#         self.ax = ax
-
-# x = Render(100)
-# x.add()
-# plt.show()
+# Matplotlib shenanigans
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
 
 
+def draw_object(ax, position, radius, color, alpha):
+    u = np.linspace(0, 2 * np.pi, 100)
+    v = np.linspace(0, np.pi, 100)
+    x = position[0] + radius * np.outer(np.cos(u), np.sin(v))
+    y = position[1] + radius * np.outer(np.sin(u), np.sin(v))
+    z = position[2] + radius * np.outer(np.ones(np.size(u)), np.cos(v))
+    ax.plot_surface(x,y,z, color, alpha, 0)
+
+
+# Set aspect ratio 
+ax.set_box_aspect([1, 1, 1])
+
+# Set axis labels
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_zlabel("Z")
+
+# Set plot limits
+ax.set_xlim([-100, 100])
+ax.set_ylim([-100, 100])
+ax.set_zlim([-100, 100])
+
+
+
+# Initialize objects
 blue_planet = SpaceObject(
     5.97 * 10 ** 24, np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), "Blue"
 )
 red_planet = SpaceObject(
     2 * 10 ** 30, np.array([149.6 * 10 ** 6, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), "Red"
 )
+
+# Add objects to system
 system = SpaceSystem([], 10)
 system.objects.append(blue_planet)
 system.objects.append(red_planet)
@@ -33,13 +52,4 @@ system.euler_method()
 print(system)
 
 
-
-
-
-fig, ax = plt.subplots(1, 1, subplot_kw={"projection": "3d"})
-ax.plot((0,0,0), marker='0',markersize=.5,color='yellow')
-
-
-# ani = animation.ArtistAnimation(fig=fig, artists=artists, interval=400)
 plt.show()
-
